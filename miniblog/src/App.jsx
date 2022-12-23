3d1cd1bd93674b54cf7378d60e260a314388d4f3
João Gomes <joaoeduardogomes@gmail.com>
2022-12-23 10:11:54 -0300

    adicionando navbar

--- miniblog/src/App.jsx
+++ miniblog/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react"
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import "./App.css"
+import Footer from "./components/Footer"
+import Navbar from "./components/Navbar"
 import About from "./pages/About/About"
 
 //pages
@@ -12,12 +14,14 @@ function App() {
 	return (
 		<div className='App'>
 			<BrowserRouter>
+			<Navbar/>
 				<div className='container'>
 					<Routes>
             <Route path="/" element={<Home/>} />
             <Route path="/about" element={<About />} />
           </Routes>
 				</div>
+				<Footer/>
 			</BrowserRouter>
 		</div>
 	)
